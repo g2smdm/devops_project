2c408728378747dde03697b80e3f7b7a3f3de4e2
gopiteekenam <g2smdm@gmail.com>
2023-07-13 08:25:18 +0530

    commit the code to subtract 2 numbers

---
+++ src/main/java/application.java
@@ -0,0 +1,13 @@
+//This class takes care of substracting 2 numbers
+
+Class Application {
+	public static void main(String args[]) {
+		System.out.println(sub(3, 6));
+	}
+	
+	int sub(int a, int b)
+	{
+		return a-b;
+	}
+}
+	
