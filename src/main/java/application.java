45dee09e0907ce13db1ca50bf20806511d5a4e20
gopiteekenam <g2smdm@gmail.com>
2023-07-13 08:40:22 +0530

    commit the changes for multiply 2 numbers

--- src/main/java/application.java
+++ src/main/java/application.java
@@ -1,9 +1,9 @@
-
 //This class takes care of substracting 2 numbers
 Class Application {
 	public static void main(String args[]) {
-		System.out.println(add(3, 6));
+	System.out.println(add(3, 6));
     System.out.println(sub(3, 6));
+	System.out.println(multiply(3,6));
 	}
 	
 	int add(int a, int b)
@@ -11,9 +11,16 @@ Class Application {
 		return a+b;
 	}
   
-  int sub(int a, int b)
+	int sub(int a, int b)
 	{
 		return a-b;
 	}
+	
+	int multiply(int a, int b)
+	{
+		return a *b;
+	}
 }
+	
+		
 	
